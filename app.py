5a17979950118d0c47ea0adeae502313e6a775e7
sourav199 <souravdhar1999@gmail.com>
2021-10-14 20:44:13 -0400

    changed the host port

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ class Userdata(db.Model, UserMixin):
     artistId = db.Column(db.String(300), unique=False)
 
 
-db.drop_all()
 db.create_all()  # table created in the databas  done
 
 
@@ -149,5 +148,5 @@ def home(em):
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+        host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 8080)), debug=True
     )
