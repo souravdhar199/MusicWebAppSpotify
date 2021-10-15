3bb97e553b339dcef716a88243249d8afce45639
sourav199 <souravdhar1999@gmail.com>
2021-10-14 21:20:59 -0400

    changed the host

--- app.py
+++ app.py
@@ -153,5 +153,5 @@ def home(em):
 
 if __name__ == "__main__":
     app.run(
-        host=os.getenv("IP", "127.0.0.1"), port=int(os.getenv("PORT", 8080)), debug=True
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
     )
